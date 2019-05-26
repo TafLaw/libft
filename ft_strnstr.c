8632ba925770e77fad466cecdaf88a9a5ff74a9b
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-05-26 09:18:04 +0200

    same as ft_strstr but reads only n bytes of the main string

---
+++ ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmuzeren <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 10:02:56 by tmuzeren          #+#    #+#             */
+/*   Updated: 2019/05/24 12:33:43 by tmuzeren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *hay, const char *needle, size_t n)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (*needle == '\0')
+		return ((char *)hay);
+	while (hay[i] != '\0' && i < n)
+	{
+		j = 0;
+		while (hay[i + j] == needle[j])
+		{
+			j++;
+			if (needle[j] == '\0')
+				return ((char *)hay + i);
+		}
+		i++;
+	}
+	return (0);
+}
