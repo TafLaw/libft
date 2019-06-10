ca3a29c2279ec9a54aaf6d1be1b122091f9d8ba5
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-06-10 16:44:16 +0200

    String compare

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,16 +6,20 @@
 /*   By: tmuzeren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:54:02 by tmuzeren          #+#    #+#             */
-/*   Updated: 2019/05/21 10:02:48 by tmuzeren         ###   ########.fr       */
+/*   Updated: 2019/05/29 17:04:55 by tmuzeren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	unsigned int i;
+	unsigned int	i;
+	unsigned char	*str;
+	unsigned char	*str2;
 
 	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i])
+	str = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (str[i] && str2[i] && str[i] == str2[i])
 		i++;
-	return (s1[i] - s2[i]);
+	return (str[i] - str2[i]);
 }
