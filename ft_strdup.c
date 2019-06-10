9c3a79adeb973d159b7764c5d46eb97648e00079
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-06-10 16:46:42 +0200

    string duplicate

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: tmuzeren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 16:06:04 by tmuzeren          #+#    #+#             */
-/*   Updated: 2019/05/20 16:23:18 by tmuzeren         ###   ########.fr       */
+/*   Updated: 2019/06/06 16:03:28 by tmuzeren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 
 char	*ft_strdup(const char *s)
 {
+	char	*ss;
 	char	*str;
 	size_t	i;
 
 	i = 0;
+	ss = (char *)s;
 	str = (char *)malloc(sizeof(str) * ft_strlen(s) + 1);
-	if (!str)
+	if (str == NULL)
 		return (NULL);
 	else
 	{
-		while (s[i] != '\0')
+		while (ss[i] != '\0')
 		{
-			str[i] = s[i];
+			str[i] = ss[i];
 			i++;
 		}
 		str[i] = '\0';
