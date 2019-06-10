db473bff39ebf97708eb7aee4d5ce3c7d7db3671
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-06-10 17:19:03 +0200

    checks if given input is an ascii

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,13 +6,15 @@
 /*   By: tmuzeren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 13:03:29 by tmuzeren          #+#    #+#             */
-/*   Updated: 2019/05/21 13:10:31 by tmuzeren         ###   ########.fr       */
+/*   Updated: 2019/06/07 15:54:44 by tmuzeren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int	ft_isascii(int c)
 {
-	if (c >= 0 && c <= 177)
+	if (c >= 0 && c <= 127)
 		return (1);
 	return (0);
 }
