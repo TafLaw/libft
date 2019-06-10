e6bd48fec972dd7c5012d677985dab9d99047b66
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-06-10 16:50:43 +0200

    length of concatenated strings

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tmuzeren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 17:17:10 by tmuzeren          #+#    #+#             */
-/*   Updated: 2019/05/20 17:25:10 by tmuzeren         ###   ########.fr       */
+/*   Updated: 2019/06/10 13:41:21 by tmuzeren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t n)
 {
-	return (ft_strlen(ft_strncat(dest, src, n)));
+	size_t	len;
+
+	len = ft_strlen(dest);
+	if (len > n)
+		return (ft_strlen(src) + n);
+	if (len < n)
+		ft_strncat(dest, src, n - (len + 1));
+	return (ft_strlen(src) + len);
 }
