735669bef6b5837105ee24b47de16cb6c48834b7
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-06-10 16:57:27 +0200

    create a new string

---
+++ ft_strnew.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmuzeren <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 15:26:29 by tmuzeren          #+#    #+#             */
+/*   Updated: 2019/05/22 17:37:45 by tmuzeren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * size + 1);
+	if (str == NULL)
+		return (NULL);
+	else
+	{
+		while (i < size)
+		{
+			str[i] = '\0';
+			i++;
+		}
+		str[i] = '\0';
+	}
+	return (str);
+}
