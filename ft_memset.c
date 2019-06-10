7ed9b5218d597517a1189d8af1f77e4bc280c1c4
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-06-10 16:33:01 +0200

    write n bytes of specified value

--- ft_memset.c
+++ ft_memset.c
@@ -6,23 +6,22 @@
 /*   By: tmuzeren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:19:49 by tmuzeren          #+#    #+#             */
-/*   Updated: 2019/05/20 12:08:46 by tmuzeren         ###   ########.fr       */
+/*   Updated: 2019/06/06 09:14:21 by tmuzeren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	size_t	i;
-	char	*str;
+	size_t			i;
+	unsigned char	*str;
 
 	i = 0;
-	str = (char *)s;
-	while (str != '\0' && i < n)
+	str = (unsigned char *)s;
+	while (i < n)
 	{
-		str[i] = c;
+		str[i] = (unsigned char)c;
 		i++;
 	}
 	return (str);
