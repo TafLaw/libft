80ab150aae42073eefc269e7c14fac0ab41337d1
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-06-10 16:55:27 +0200

    copy n bytes of string

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: tmuzeren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 16:44:39 by tmuzeren          #+#    #+#             */
-/*   Updated: 2019/05/20 16:50:56 by tmuzeren         ###   ########.fr       */
+/*   Updated: 2019/05/29 12:41:10 by tmuzeren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,22 @@
 
 char	*ft_strncpy(char *dest, const char *src, size_t n)
 {
+	size_t	x;
 	size_t	i;
 
+	x = 0;
 	i = 0;
-	while (src[i] && i < n)
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return (0);
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	x = ft_strlen(dest) - i;
+	dest[i + x] = '\0';
+	return (dest);
 }
